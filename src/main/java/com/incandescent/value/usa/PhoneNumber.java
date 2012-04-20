889aa0e97fe5866a9c4322cc0e751071b2b12b4c
Brad Neighbors <brad.r.neighbors@gmail.com>
2012-04-20 16:48:22 -0400

    Added american phone number

---
+++ src/main/java/com/incandescent/value/usa/PhoneNumber.java
@@ -0,0 +1,15 @@
+package com.incandescent.value.usa;
+
+import com.incandescent.value.StringValue;
+
+public class PhoneNumber extends StringValue {
+
+    protected PhoneNumber(String value) {
+        super(value);
+        final String strippedVal = value.replace(" ", "").replace("(", "").replace(")", "").replace("-", "").trim();
+        if (!strippedVal.matches("\\d{10}?")) {
+            throw new IllegalArgumentException("Not a valid American phone number.");
+        }
+        super.value = strippedVal;
+    }
+}
